04fa53168b550b991c7fa3dc02959ef8ab05eccd
Shivansh Tuteja <tuteja101@gmail.com>
2024-11-05 00:17:31 +0530

    error handling using http404

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
 from .models import Question
 # from django.template import loader
 
@@ -13,7 +15,11 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.query.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("question does not exist")
+    return render(request, "polls/detail.html", {"question" : question})a
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}."
