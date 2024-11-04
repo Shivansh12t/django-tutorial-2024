5d30cb5f66f9cea1f396e79374943398ec4e7861
Shivansh Tuteja <tuteja101@gmail.com>
2024-11-04 23:46:05 +0530

    new style string formatting

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -6,11 +6,11 @@ def index(request):
 # Create your views here.
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    return HttpResponse(f"You're looking at question {question_id}.")
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    response = "You're looking at the results of question {question_id}."
+    return HttpResponse(response)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("You're voting on question {question_id}.")
