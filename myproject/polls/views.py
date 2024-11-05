5edb2dd93008e7f1e83d1fffe4e201d7058a9791
Shivansh Tuteja <tuteja101@gmail.com>
2024-11-05 18:51:10 +0530

    fixed errors

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -34,14 +34,11 @@ def results(request, question_id):
     return render(request, "polls/results.html", {"question" : question})
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, ph = question_id)
+    question = get_object_or_404(Question, pk = question_id)
     try:
         selected_choice = question.choice_set.get(pk = request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(
-            request,
-            "polls/details.html",
-            {
+        return render(request, "polls/detail.html", {
                 "question" : question,
                 "error_message" : "You didn't select a choice",
             },
